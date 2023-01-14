c7224c8e4c9988dc285757358b1639f6135f63ed
Darshan Pawar <darshanpawar010@gmail.com>
2023-01-14 14:20:04 +0530

    APIs are Updated

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const os = require('os');
 
-app.get('/users/:username', async (req, res) => {
-    const gitapiurl = `https://api.github.com/users/${req.params.username}`;
-    const response = await fetch(gitapiurl);
-    const jsonValue = await response.json();
-    res.json(jsonValue);
+console.log("HostName = ",os.hostname());
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Github Clone");
 })
 
-
-app.get('/repos/:username', async (req, res) => {
-    const pageno = req.query.page ? req.query.page : 1;
-    const gitapiurl = `https://api.github.com/users/${req.params.username}/repos?per_page=10&page=${pageno}`;
-    const response = await fetch(gitapiurl);
+app.get('/users', async (req, res) => {
+    const github_api_users_url = `https://api.github.com/users`;
+    const response = await fetch(github_api_users_url);
     const jsonValue = await response.json();
-    res.json(jsonValue);
-    // res.send(`${req.params.page} & ${gitapiurl} & ${pageno}`)
+    res.status(response.status).json(jsonValue);
+})
+
+app.get('/users/:username', async (req, res) => {
+  console.log("username = ",req.params.username);
+    const github_api_username_url = `https://api.github.com/users/${req.params.username}`;
+    const response = await fetch(github_api_username_url);
+    const jsonValue = await response.json();
+    res.status(response.status).json(jsonValue);
+})
+
+app.get('/users/:username/repos', async (req, res) => {
+  console.log("username = ",req.params.username);
+    const pageno = req.query.page ? req.query.page : 1;
+    const github_api_repos_url = `https://api.github.com/users/${req.params.username}/repos?per_page=10&page=${pageno}`;
+    const response = await fetch(github_api_repos_url);
+    const jsonValue = await response.json();
+    res.status(response.status).json(jsonValue);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Node App listening on port ${port}... click on http://localhost:${port}/ or click on http://${os.hostname()}:${port}/`)
 })
